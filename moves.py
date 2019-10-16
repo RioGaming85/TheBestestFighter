7729501a6862615eb383a1dfa4ca80915cf7a862
RioGaming85 <43646340+RioGaming85@users.noreply.github.com>
2019-10-16 10:05:18 +0100

    Implemented the 'Ult' move

--- moves.py
+++ moves.py
@@ -2,13 +2,14 @@ from random import randint
 from player import Player
 
 class Move:
-  def __init__(self, name, chance, getDamage, getSpeed = 1):
+  def __init__(self, name, chance, getDamage, getSpeed):
     self.name = name
     self.chance = chance
     self.getDamage = getDamage
     self.getSpeed = getSpeed
 
   def miss(self, target, player):
+    print(f'Player {player.name}\'s {self.name} missed!')
     pass
 
   def hit(self, target, player):
@@ -22,16 +23,21 @@ class Move:
 
 
 class Headbutt(Move):
-  def __init__(self, chance, getDamage):
-    super(Headbutt, self).__init__('headbutt', chance, getDamage)
+  def __init__(self, chance, getDamage, getSpeed):
+    super(Headbutt, self).__init__('headbutt', chance, getDamage, getSpeed)
 
   def miss(self, target, player):
   # player.health = player.health * (2 / 3)
     player.damageTaken = 1.5
 
+class Ult(Move):
+  def __init__(self, chance, getDamage, getSpeed):
+    super(Ult, self).__init__(self, chance, getDamage, getSpeed)
+    Player.ultCharge = 0
+
 class Block(Move):
-  def __init__(self, chance):
-    super(Block, self).__init__('block', chance, lambda: 0)
+  def __init__(self, chance, getSpeed):
+    super(Block, self).__init__('block', chance, lambda: 0, getSpeed)
 
   def hit(self, target, player):
     Player.damageTaken = randint(3, 7) / 10
