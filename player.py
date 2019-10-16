fc1432b41a261e21092ca00f5bf5a1d38ae399bc
RioGaming85 <43646340+RioGaming85@users.noreply.github.com>
2019-10-16 10:04:20 +0100

    Made some UI changes

--- player.py
+++ player.py
@@ -1,11 +1,10 @@
-from random import randint
+import math
 
-def getMaxHealth():
-  return randint(20, 30)
+MAX_HEALTH = 25
 
 class Player:
   def __init__(self, name, moves):
-    self.health = getMaxHealth()
+    self.health = MAX_HEALTH
     self.name = name
     self.moves = moves
     self.damageTaken = 1
@@ -14,23 +13,25 @@ class Player:
 
   def takeDamage(self, d):
     damage = d * self.damageTaken
+    damage = math.ceil(damage)
     self.health -= damage
     self.damageTaken = 1
     self.ultCharge = min(1, self.ultCharge + (damage / 50))
     if self.health <= 0:
       self.health = 0
-    print(f"Player {self.name}: Took {damage}, HP {self.health}")
+    print(f"Player {self.name}: Took {damage} damage! {self.health} HP remaining")
 
   def log(self, text):
     print(f"Player {self.name}: {text}")
 
   def prompt(self):
+    print('\n')
     if self.health <= 0:
       self.health = 0
-    print(f"Player {self.name}: HP {self.health}")
+    print(f"Player {self.name}: HP {self.health}\n-------------------------------------------")
     for i, move in enumerate(self.moves):
       print(f"{i} {move.name}")
-    selection = input('> ')
+    selection = input(f"-------------------------------------------\nPlayer {self.name} move choice:\n > ")
     mid = 0
     try:
       mid = int(selection)
