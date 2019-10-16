efab1d7f6c96486cd4bd151a054d1367d736809c
RioGaming85 <43646340+RioGaming85@users.noreply.github.com>
2019-10-16 10:03:09 +0100

    Change UI and implement more features
     - Add win/loss/draw states
     - Implemented Ultimate move

--- main.py
+++ main.py
@@ -1,28 +1,64 @@
-import random
+from random import randint
 import moves
-from player import getMaxHealth, Player
+from player import Player
 
-punch = moves.Move('punch', 1, lambda: random.randint(3, 6))
-kick = moves.Move('kick', 0.70, lambda: random.randint(5,9))
-headbutt = moves.Headbutt(0.25, getMaxHealth)
-block = moves.Block(1)
-ult = moves.Move('ultimate',2,lambda: random.randint(11, 15))
+punch = moves.Move('punch', 1, lambda: randint(3, 6), 3)
+kick = moves.Move('kick', 0.70, lambda: randint(7, 10), 2)
+headbutt = moves.Headbutt(0.25, randint(15, 20), 1)
+block = moves.Block(1, 5)
+ult = moves.Ult(1, lambda: randint(12,17), 4)
 
-def process(target, player):
-  if player.health <= 0:
-    print(f'Player {player.name} has died!\nPlayer {target.name} wins!')
-    return None
-  move = player.prompt()
-  move.run(target, player)
+def process(player1, player2):
+  if player1.ultCharge >= 5:
+    player1.moves = [punch, kick, block, headbutt, ult]
+  else:
+    player1.moves = [punch, kick, block, headbutt]
+  if player2.ultCharge >= 5:
+    player2.moves = [punch, kick, block, headbutt, ult]
+  else:
+    player2.moves = [punch, kick, block, headbutt]
+  move1 = player1.prompt()
+  move2 = player2.prompt()
+
+  print('\n-------------------------------------------')
+  if move1.getSpeed > move2.getSpeed:
+      print(f'Player {player1.name} moves first!')
+      move1.run(player2, player1)
+      move2.run(player1, player2)
+  elif move2.getSpeed > move1.getSpeed:
+      print(f'Player {player2.name} moves first!')
+      move2.run(player1, player2)
+      move1.run(player2, player1)
+  else:
+      if randint(0, 1) == 0:
+          print(f'Player {player1.name} moves first!')
+          move1.run(player2, player1)
+          move2.run(player1, player2)
+      else:
+          print(f'Player {player2.name} moves first!')
+          move2.run(player1, player2)
+          move1.run(player2, player1)
+  print('-------------------------------------------')
+
 
 def main():
-  moves = [punch, kick, block, headbutt]
-  player1 = Player('1', moves)
-  player2 = Player('2', moves)
+    moves = [punch, kick, block, headbutt]
+    player1 = Player('1', moves)
+    player2 = Player('2', moves)
 
-  while True:
-    process(player2, player1)
-    process(player1, player2)
-    if player1.health <= 0 or player2.health <= 0:
-      break
-main()
+    while True:
+        if player1.health <= 0 and player2.health <= 0:
+          print(f'Player {player1.name} and Player {player2.name} have KO\'d each other at the same time!')
+          print('It\'s a draw!')
+          break
+        if player1.health <= 0:
+          print(f'Player {player1.name} has died!\nPlayer {player2.name} wins!')
+          break
+        elif player2.health <= 0:
+          print(f'Player {player2.name} has died!\nPlayer {player1.name} wins!')
+          break
+        process(player1, player2)
+
+
+if __name__ == "__main__":
+    main()
